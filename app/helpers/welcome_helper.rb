0d2dca4af028a474dbde34888724f6f5610cd1e3
Aaron Goldsmith <aaron.goldsmith@damballa.com>
2014-03-27 03:46:41 -0400

    Add the wunderground view.

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,3 +1,5 @@
+require 'wunderground_client'
+
 module WelcomeHelper
   def pref_has_keys?(plugin_name, keys)
     Preference.pref_has_keys?(plugin_name, keys)
@@ -5,5 +7,10 @@ module WelcomeHelper
 
   def safe_pref_fetch(plugin_name, key)
     Preference.safe_pref_fetch(plugin_name, key)
+  end
+
+  def get_weather
+    client = WundergroundClient.new
+    return client.current, client.forecast
   end
 end
